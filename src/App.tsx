063581c432234386ee380478ace6f8d8a98dab97
Osaro Iyoha <saro.iyoha@gmail.com>
2024-05-03 12:19:53 +0100

    add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 import illustrationDesktop from './assets/images/illustration-sign-up-desktop.svg';
 import illustrationMobile from './assets/images/illustration-sign-up-mobile.svg';
@@ -7,25 +7,28 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <div className="left">
-      <h1>Stay updated!</h1>
-      <p>Join 60,000+ product managers receiving monthly updates on:</p>
-      <ul>
-        <li>Product discovery and building what matters</li>
-        <li>Measuring to ensure updates are a success</li>
-        <li>And much more!</li>
-      </ul>
-
-      <form action="">
-        <label htmlFor="email_field">Email address</label>
-        <input type="text" id='email_field' placeholder='email@company.com'/>
-        <input type="button" value="Subscribe to monthly newsletter" />
-      </form>
-    </div>
-    <div className="right">
-      <img src={illustrationDesktop} alt="illustration" />
-    </div>
+      <main className="card">
+        <div className="card__inner">
+          <div className="left">
+          <h1>Stay updated!</h1>
+          <p className="card__copy">Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul className="card__copy--list">
+            <li><p>Product discovery and building what matters</p></li>
+            <li><p>Measuring to ensure updates are a success</p></li>
+            <li><p>And much more!</p></li>
+          </ul>
+          
+          <form action="" className="card__form">
+            <label htmlFor="email_field">Email address</label>
+            <input type="text" id='email_field' placeholder='email@company.com'/>
+            <input type="button" value="Subscribe to monthly newsletter" />
+          </form>
+          </div>
+          <div className="right">
+            <img src={illustrationDesktop} alt="illustration" />
+          </div>
+        </div>
+    
      {/* Sign-up form start */}
      
 
@@ -40,7 +43,7 @@ Please open it and click the button inside to confirm your subscription.
 Dismiss message
 
 <!-- Success message end --> */}
-    </>
+</main>
   )
 }
 
